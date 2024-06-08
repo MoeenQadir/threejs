4f8bd0a7b446231db5b4d7ae6141e44d4ac30986
MoeenQadir <myselfxdeveloper@gmail.com>
2024-06-08 18:34:22 +0500

    shit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls, Environment, Html } from '@react-three/drei';
 import { animated, useSpring, config } from '@react-spring/three';
 import * as THREE from 'three';
-import './App.css'; // Assuming you have Tailwind CSS setup in App.css
+import './App.css';
 
 const objects = [
     { id: 1, size: 0.5, position: { x: 0, y: 0.5, z: 0 }, connections: [2, 3, 5] },
@@ -34,7 +34,7 @@ function Node({ size, position, id }) {
     return (
         <group>
             <animated.mesh ref={mesh} position={[position.x, position.y, position.z]} scale={scale} castShadow receiveShadow>
-                <cylinderGeometry args={[1.5, 1.5, 1, 62]} />
+                <cylinderGeometry args={[1, 1, 1, 62]} />
                 <meshStandardMaterial color="red" metalness={1} roughness={0.1} />
                 <Html position={[0, 0.6, 0]} center>
                     <div style={{ color: 'white', fontSize: '10px', textAlign: 'center', whiteSpace: 'nowrap', pointerEvents: 'none' }}>
